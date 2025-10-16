22e46ae182b216b266a9ed75f029f36c616cf3fd
Anggipurwanti15 <purwantia091@gmail.com>
2025-10-16 21:53:09 +0700

    Rest Client

--- rest-client2/index.php
+++ rest-client2/index.php
@@ -18,32 +18,65 @@ $hasil=json_decode($data,true);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Rest Client dengan cURL</title>
+    <title>Rest Client</title>
     <!-- CSS Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-</head>
+<head>
+    <title>Rest Client</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <style>
+        /* Warna Latar Belakang abu-abu */
+        .navbar-custom {
+            background-color: #767c7cff !important; /* Warna Teal gelap */
+        }
+
+        /* Memastikan teks tetap putih/terang */
+        .navbar-custom .navbar-brand,
+        .navbar-custom .nav-link {
+            color: white !important;
+        }
+        
+        /* Ikon Home terlihat lebih jelas */
+        .navbar-custom .nav-link i {
+            margin-right: 5px;
+        }
+    </style>
 <body>
  
 <!-- navbar -->
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-info">
-  <a class="navbar-brand" href="#">RestClient</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">News</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark navbar-custom">
+  <div class="container"> 
+    
+    <a class="navbar-brand" href="#">
+        <i class="fas fa-newspaper"></i> Rest Client
+        </a>
+    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <i class="fas fa-home"></i> Home
+            </a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">News</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+    </div>
+    
+  </div> </nav>
 <!-- navbar -->
 <div class="container" style="margin-top: 75px;">
     <div class="row">
